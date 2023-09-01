084d5df382f0df23b54ce72de86f994bb4ef5230
fahimmutashir23 <taslimit23@gmail.com>
2023-09-01 21:14:55 +0600

    added new sorting methode

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const phTube = async () => {
   categories.forEach((categorie) => {
     const div = document.createElement("div");
     div.innerHTML = `
-            <button  onclick= "allShow('${categorie.category_id}')", sortResult('${categorie.category_id}') class="btn text-base font-medium px-5 w-24 focus:outline-none focus:bg focus:bg-red-600 focus:text-white">${categorie.category}</button>
+            <button  onclick= "allShow('${categorie.category_id}')" class="btn text-base font-medium px-5 w-24 focus:outline-none focus:bg focus:bg-red-600 focus:text-white">${categorie.category}</button>
          `;
     categoriesSection.appendChild(div);
   });
@@ -20,14 +20,20 @@ const phTube = async () => {
 
 //----------------------------------------All Show section---------------------------------
 
-const allShow = async (id) => {
+const allShow = async (id, sort) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${id}`
   );
   const data = await res.json();
   const allData = data.data;
   const status = data.status;
-  
+
+//---------------------data sorting-------------------------------------
+
+  if(sort){
+    allData.sort((a, b) => b.others.views.slice(0, -1) - a.others.views.slice(0, -1)) 
+}
+
 //----------------------data not found-----------------------------------
 
   const noReault = document.getElementById('no-result');
@@ -96,61 +102,8 @@ const allShow = async (id) => {
 
 //-----------------------------------sort result-------------------------------------------
 
-const sortResult = async(id) =>{
-    const res = await fetch`https://openapi.programming-hero.com/api/videos/category/1000`;
-    const data = await res.json();
-    const fInalData = data.data;
-    
-    const sortNum = fInalData.sort((a, b) => b.others.views.slice(0, -1) - a.others.views.slice(0, -1))  
-
-    const videoContainer = document.getElementById("video-container");
-    videoContainer.textContent = ""
-    
-    sortNum.forEach((sortData) => {
-        const sec = parseInt(sortData.others.posted_date); 
-    let hours   = Math.floor(sec / 3600);
-    let minutes = Math.floor((sec - (hours * 3600)) / 60);
-    
-   //-----------------create cart------------------------------
-
-    const cart = document.createElement("div");
-    cart.innerHTML = `
-        <div class="card  bg-base-100 shadow-xl">
-                    <div class = "p-3 relative">
-                      <img class = "h-48 mx-auto rounded-md"
-                        src="${sortData.thumbnail}"
-                      />
-                      <p class ="text-xs p-1 rounded-xl absolute bottom-4 right-5 bg-slate-800 text-white">
-                      ${sortData.others.posted_date? `${hours} hrs ${minutes} min ago` : ""}
-                      </p>
-                    </div>
-                    <div class="p-3">
-                        <div class="flex gap-3">
-                            <div class="">
-                                <img class="w-10 h-10 rounded-full"
-                                  src="${sortData.authors[0]?.profile_picture}"
-                                />
-                              </div>
-                          <h2 class="card-title text-base font-bold">
-                            ${sortData.title}
-                          </h2>
-                        </div>
-                        <div class = "flex gap-2 my-2">
-                        <p class="text-sm font-normal">
-                        ${sortData.authors[0]?.profile_name}
-                        <p>
-                        <span>${sortData.authors[0].verified? `<img class="w-5"
-                        src="veryfied.png"
-                        />` : ""}
-                        </span> 
-                        </div>                     
-                        <p class="text-sm font-normal my-3">total view : ${sortData.others?.views}</p>
-                    </div>
-                  </div>
-        `;
-    videoContainer.appendChild(cart);
-    })
-    
+const sortResult = async() =>{
+    allShow('1000', true)    
 }
 
 allShow('1000')
